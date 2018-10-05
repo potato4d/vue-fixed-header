a86b5fd4a9daef98c2f3b8fcd608788f6a34c198
HANATANI Takuma <mail@potato4d.me>
2018-10-05 14:15:30 +0900

    Replace to TypeScript

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import { Component, Prop } from 'vue-property-decorator'
+
+function getTargetTag() {
+  if (navigator.userAgent.includes('Edge')) return 'body'
+  if (!(window as any).chrome && 'WebkitAppearance' in (document.documentElement as any).style) return 'body'
+  return 'html'
+}
+
+@Component
+export default class VueFixedHeader extends Vue {
+  $: (selector: string) => any = () => null
+  check: () => void = () => null
+  tag: string | null = null
+  @Prop({ default: 0, type: Number}) threshold!: number
+
+  mounted () {
+    this.$ = (e: string) => document.querySelector(e)
+    this.tag = getTargetTag()
+    this.main()
+  }
+
+  main () {
+    this.check = () => {
+      const { $, tag, threshold } = this
+      if (!tag) {
+        return
+      }
+      this.$emit('update:fixed', $(tag).scrollTop > threshold)
+    }
+    window.addEventListener('scroll', this.check)
+  }
+
+  beforeDestroy () {
+    window.removeEventListener('scroll', this.check)
+  }
+
+  render() {
+    const children = this.$slots.default
+    return children[0]
+  }
+}

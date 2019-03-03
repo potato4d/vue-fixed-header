be6e07a939e0ce480481d942679f27bcc1649b92
HANATANI Takuma <mail@potato4d.me>
2019-03-03 23:45:04 +0900

    doc: Add VuePress document

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  title: 'vue-fixed-header',
+  description: 'Simple and cross-browser friendly fixed header component for Vue.js.',
+  themeConfig: {
+    nav: [
+      { text: 'About', link: '/about/' },
+      { text: 'API', link: '/api/' },
+      { text: 'Example', link: '/example/' },
+    ],
+    sidebar: {
+      '/about/': [
+        '/about/'
+      ],
+      '/api/': [
+        '/api/',
+        '/api/vue-fixed-header.md',
+        '/api/v2-fixed-header.md',
+      ]
+    },
+    repo: 'potato4d/vue-fixed-header',
+    repoLabel: 'GitHub',
+    docsRepo: 'potato4d/vue-fixed-header',
+    docsDir: 'docs',
+    editLinks: true
+  }
+}
